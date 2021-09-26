42d3800f5f8baf5859470f509a03a3b8d6b70aa0
mortem45 <ste45_007@hotmail.com>
2021-09-25 23:57:32 -0600

    agragando archivos

---
+++ src/components/salarios.js
@@ -0,0 +1,81 @@
+const guatemala = [];
+guatemala.push({
+  name: "Kelin",
+  salary: 500,
+});
+guatemala.push({
+  name: "Liseth",
+  salary: 1500,
+});
+guatemala.push({
+  name: "Luisa",
+  salary: 1800,
+});
+guatemala.push({
+  name: "Fernanda",
+  salary: 1000,
+});
+guatemala.push({
+  name: "Daniela",
+  salary: 2200,
+});
+guatemala.push({
+  name: "Rodrigo",
+  salary: 200,
+});
+guatemala.push({
+  name: "Carlos",
+  salary: 500,
+});
+guatemala.push({
+  name: "Antonio",
+  salary: 1500,
+});
+guatemala.push({
+  name: "Alicia",
+  salary: 1300,
+});
+guatemala.push({
+  name: "Ains",
+  salary: 2400,
+});
+guatemala.push({
+  name: "Rem",
+  salary: 3400,
+});
+guatemala.push({
+  name: "Rias",
+  salary: 400,
+});
+guatemala.push({
+  name: "Akeno",
+  salary: 400,
+});
+guatemala.push({
+  name: "Mai",
+  salary: 400,
+});
+guatemala.push({
+  name: "Emma",
+  salary: 600,
+});
+guatemala.push({
+  name: "Hanagaki",
+  salary: 1600,
+});
+guatemala.push({
+  name: "Draken",
+  salary: 2600,
+});
+guatemala.push({
+  name: "Chifuyu",
+  salary: 1000,
+});
+guatemala.push({
+  name: "Mikasa",
+  salary: 2000,
+});
+guatemala.push({
+  name: "Yukky",
+  salary: 100000000,
+});
